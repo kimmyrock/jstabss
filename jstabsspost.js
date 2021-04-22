5ed73249a7afa309eceac2d646daa744ee218cb0
kimmyrock <kimyrockzz@gmail.com>
2021-04-22 14:54:47 +0800

    Update jstabsspost.js

--- jstabsspost.js
+++ jstabsspost.js
@@ -1,21 +1,3 @@
-   <!-- =============== TAB JS =============== -->
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-   <!-- =============== TAB JS =============== -->
 
    <!-- =============== TAB JS =============== -->
 function openCity(evt, cityName) {
